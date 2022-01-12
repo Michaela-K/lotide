46a0a701bb0f943dc3a8acdafd14b0705a756b8c
Michaela-K <michaelakodes@gmail.com>
2022-01-12 13:00:09 +0000

    assertArraysEqual update

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,32 +1,22 @@
-const assertEqual = function(eq, bool) {
-  if (eq === bool) {
-    console.log(`Assertion Passed: ${eq} === ${bool}`);
+const eqArrays = function(arr1, arr2) {
+  let test = arr1.every(function(val, index) {
+    return val === arr2[index];
+  });
+  if (Array.isArray(arr1) && Array.isArray(arr2) && arr1.length === arr2.length && test === true) {
+    return true;
   } else {
-    console.log(`Assertion Failed: ${eq} !== ${bool}`);
+    return false;
   }
 };
 
-let output;
-const eqArrays = function(arr1, arr2){
-  for (var i = 0; i < arr1.length; i++) {
-    // console.log(`arr1 is: ${arr1[i]}`);
-  }
-  for (var j = 0; j < arr2.length; j++){
-    // console.log(`arr2 is: ${arr2[j]}`);
-  }
-  if (arr1[i] !== arr2[j]){
-    output = false;
-  }else{
-    output = true;
-  }
-  return output;
-}
-
-const assertArraysEqual = function(a1, a2){
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+const assertArraysEqual = function(a1, a2) {
+  //use eqArrays for comparison
+  if (a1 === a2) {
+    console.log(`Assertion Passed: ${a1} === ${a2}`);
   } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`Assertion Failed: ${a1} !== ${a2}`);
   }
 };
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertArraysEqual(eqArrays([1, 2, 3], [1, 7, 3]), true);
