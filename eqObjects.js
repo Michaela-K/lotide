9d5dec3ea09f0fe1cae25a71def0372d6188ce7b
Michaela-K <michaelakodes@gmail.com>
2022-01-14 02:39:14 +0000

    map.js

--- eqObjects.js
+++ eqObjects.js
@@ -6,10 +6,18 @@ const assertEqual = function (actual, expected) {
     console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
+const eqArrays = function(arr1, arr2) {
+  let test = arr1.every(function(val, index) {
+    return  val  ===  arr2[index];
+  });
+  if (arr1.length === arr2.length && test === true) {
+    return true;
+  } else {
+    return false;
+  }
+};
 const eqObjects = function (obj1, obj2) {
   //to compare obj data? - using obj key iteration
-  let output;
   //returns true (if both objects have identical keys with identical values)
   let keyArr1 = Object.keys(obj1);
   let keyArr2 = Object.keys(obj2);
@@ -17,18 +25,24 @@ const eqObjects = function (obj1, obj2) {
     return false;
   }
   for (const key of keyArr1) {
+    // console.log(obj1[key], obj2[key]);
+    // console.log()
+    if(Array.isArray(obj1[key]) && Array.isArray(obj2[key])){
+      console.log(eqArrays(obj1[key], obj2[key]));
+     return eqArrays(obj1[key], obj2[key]);
+    }
     if(obj1[key] !== obj2[key]){
       return false;
     }
   }
   return true;
 };
-
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// assertEqual(eqObjects(ab, ba), true); // => true
 // const abc = { a: "1", b: "2", c: "3" };
 // eqObjects(ab, abc); // => false
-// TEST CODE
-// assertEqual("Lighthouse Labs","Bootcamp");
-// assertEqual(1,1);
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
--- map.js
+++ map.js
@@ -1,11 +1,10 @@
-// const words = ["ground", "control", "to", "major", "tom"];
-
-// const map1 = words.map(function (h, i, arr) { 
-//                   return (words);   //return new array
-//                 });
-// console.log(map1); 
 const eqArrays = function(arr1, arr2) {
   let test = arr1.every(function(val, index) {
+    // console.log(arr1);           //[ 'grounderer', 'controlerer', 'strollerer', 'rollerer' ]
+    // console.log(val);            //grounderer 
+    // console.log(arr1[index]);    //grounderer
+    // console.log(arr2);           //undefined*****
+    // console.log(arr2[index]);    //ERROR********
     return val === arr2[index];
   });
   if (Array.isArray(arr1) && Array.isArray(arr2) && arr1.length === arr2.length && test === true) {
@@ -17,7 +16,6 @@ const eqArrays = function(arr1, arr2) {
 
 const assertArraysEqual = function(a1, a2) {    //to test middle function that return arrays.
   //use eqArrays for comparison
-  console.log(a1);
   if (eqArrays(a1, a2)) {
     console.log(`Assertion Passed: `, a1, "===", a2);
   } else {
@@ -25,14 +23,32 @@ const assertArraysEqual = function(a1, a2) {    //to test middle function that r
   }
 };
 
-//LHL
-const words = ["ground", "control", "to", "major", "tom"];
-//two arg - an array, a callback
+
 const map = function(array, callback) {
   const results = [];
+  array = array.flat();
   for (let item of array) {
-    results.push(callback(item));
+    results.push(cally(item));
   }
   return results;
 }
-console.log(map);
+//callback
+const cally = (function(item){ 
+return `${item}er`; 
+}); 
+
+// console.log(map(words));
+
+// TESTS
+// single array
+// words = (["ground", "control", "stroll", "roll"]);
+// words2 = (['grounder', 'controler', 'stroller', 'roller']);
+// assertArraysEqual(map(words), words2);
+// nested array
+// words = (["ground", "control", "stroll", ["roll"]]);
+// words2 = (['grounder', 'controler', 'stroller', 'roller']);
+// assertArraysEqual(map(words), words2);
+// //different nested array
+words = (["ground", "control", "stroll", [["roll"]]]);
+words2 = (['grounder', 'controler', 'stroller', 'roller']);
+assertArraysEqual(map(words), words2);
