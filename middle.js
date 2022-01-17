5af184559849a07391cc537df966d041235aa234
Michaela-K <michaelakodes@gmail.com>
2022-01-17 11:43:53 +0000

    refactored middle.js and created test

--- middle.js
+++ middle.js
@@ -1,23 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-  let test = arr1.every(function(val, index) {
-    return val === arr2[index];
-  });
-  if (Array.isArray(arr1) && Array.isArray(arr2) && arr1.length === arr2.length && test === true) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const assertArraysEqual = function(a1, a2) {    //to test middle function that return arrays.
-  //use eqArrays for comparison
-  console.log(a1);
-  if (eqArrays(a1, a2)) {
-    console.log(`Assertion Passed: `, a1, "===", a2);
-  } else {
-    console.log(`Assertion Failed: `, a1, "!==", a2);
-  }
-};
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 
 //middle - take in an array and return the middle-most element(s) of the given array.
 // ACTUAL FUNCTION
@@ -32,12 +14,4 @@ const middle = function(array) {
   }
 };
 
-console.log((middle([1]))); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-
-const middy = [1, 2, 3, 4, 5, 6];
-assertArraysEqual(middle(middy), [4, 4]);
+module.exports = middle;
