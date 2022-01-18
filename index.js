467911b496cec0736dac386c66947892cafefb07
Michaela-K <michaelakodes@gmail.com>
2022-01-18 03:19:39 +0000

    update obj shotrhand index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const head   = require('./head');
-const tail   = require('./tail');
+const head = require('./head');
+const tail = require('./tail');
 const middle = require('./middle');
 const countLetters = require("./countLetters");
 const countOnly = require("./countOnly");
@@ -16,22 +16,22 @@ const assertArraysEqual = require("./assertArraysEqual");
 const assertObjectsEqual = require("./assertObjectsEqual");
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  letterPositions: letterPositions,
-  map: map, 
-  takeUntil: takeUntil,
-  without: without,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  assertEqual: assertEqual,
-  assertArraysEqual: assertArraysEqual,
-  assertObjectsEqual: assertObjectsEqual,
+  head,
+  tail,
+  middle,
+  countLetters,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map, 
+  takeUntil,
+  without,
+  eqArrays,
+  eqObjects,
+  assertEqual,
+  assertArraysEqual,
+  assertObjectsEqual,
 };
 
 //open the node REPL
